099c9ed7a8da22115a6ab12c7f72b82db6322f48
Karen <kjsonnenberg@me.com>
2022-12-01 12:48:55 -0500

    back to start

--- azureproject/production.py
+++ azureproject/production.py
@@ -3,12 +3,12 @@ import os
 
 
 
-# ALLOWED_HOSTS = ['papermasters.azurewebsites.net', 'https://www.papermasters.com', 'papermasters.com', 'www.papermasters.com', '20.119.144.5']
-# CSRF_TRUSTED_ORIGINS = ['https://papermasters.com']
+ALLOWED_HOSTS = ['papermasters.azurewebsites.net', 'https://www.papermasters.com', 'papermasters.com', 'www.papermasters.com', '20.119.144.5']
+CSRF_TRUSTED_ORIGINS = ['https://papermasters.com']
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['papermasters.com']] if 'papermasters.com' in os.environ else []
-CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['papermasters.com']] if 'papermasters.com' in os.environ else []
+# ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else ['*']
+# CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = True
 
 # WhiteNoise configuration
